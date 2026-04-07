369c69dd437cec970cb1ee3e63d84ca8ab625668
Felix Hohn <felixmhohn@gmail.com>
2026-04-07 14:39:42 +0200

    feat: Initialize iOS project structure and assets
    - Added Xcode workspace and project files for iOS.
    - Created AppDelegate and SceneDelegate for Flutter integration.
    - Implemented launch screen and main storyboard.
    - Added app icons and launch images in asset catalog.
    - Configured Info.plist with necessary keys for app functionality.
    - Set up RunnerTests with a basic test case.
    - Updated Supabase configuration for external authentication with Google and Apple.

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/fhoh/Development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/fhoh/Git/palast-ai"
+export "FLUTTER_ROOT=/Users/felixhohn/fvm/versions/stable"
+export "FLUTTER_APPLICATION_PATH=/Users/felixhohn/Git/palast-ai"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=0.1.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuNDEuMg==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049OTA2NzNhNGVlZg==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049NmMwYmFhZWJmNw==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMS4w"
 export "DART_OBFUSCATION=false"
-export "TRACK_WIDGET_CREATION=true"
+export "TRACK_WIDGET_CREATION=false"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=/Users/felixhohn/Git/palast-ai/.dart_tool/package_config.json"
